16b15a67933f6fdd8660b39b68ef1f4cdb289525
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-10-12 16:46:12 +0200

    Add skeleton for status service

--- backend/roon_init.js
+++ backend/roon_init.js
@@ -1,15 +1,22 @@
-var RoonApi = require("node-roon-api");
+const RoonApi = require("node-roon-api");
+const RoonApiStatus = require("node-roon-api-status");
 
-var roon = new RoonApi({
-  extension_id:        'com.roon-remote.test',
-  display_name:        "Roon Web Remote Extension",
-  display_version:     "0.0.0",
-  publisher:           'Michael Pfeifer',
-  email:               'michael.k.pfeifer@googlemail.com',
-  website:             'https://github.com/michaelkpfeifer'
+const roon = new RoonApi({
+  extension_id: "com.roon-remote.test",
+  display_name: "Roon Web Remote Extension",
+  display_version: "0.0.0",
+  publisher: "Michael Pfeifer",
+  email: "michael.k.pfeifer@googlemail.com",
+  website: "https://github.com/michaelkpfeifer",
 });
 
-roon.init_services({});
+const svc_status = new RoonApiStatus(roon);
+
+roon.init_services({
+  provided_services: [svc_status],
+});
+
+svc_status.set_status("All is good", false);
 
 roon.start_discovery();
 
