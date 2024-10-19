f748ea88a448c77876b291f188d2ea351ea3079d
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-10-19 18:02:39 +0200

    Copy index.html to the dist directory during build

--- frontend/build.js
+++ frontend/build.js
@@ -1,4 +1,13 @@
 const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require('path');
+
+const distDir = path.join(__dirname, 'dist');
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir, { recursive: true });
+}
+
+fs.copyFileSync('templates/index.html', 'dist/index.html');
 
 esbuild.build({
   entryPoints: ['src/index.js'],
