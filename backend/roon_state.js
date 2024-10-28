e4e31d4f93cf955ed9f09a6b7092845b580a834f
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-10-28 21:16:22 +0100

    Emit zonesChanged message

--- backend/roon_state.js
+++ backend/roon_state.js
@@ -67,21 +67,45 @@ const zoneSubscriptionMessageHandler = (cmd, snakeCaseData) => {
           case 'zonesSeekChanged':
             // console.log(
             //   'roon_state.js: processing zonesSeekChanged message: data[attr]:',
-            //   data[attr],
+            //   JSON.stringify(data[attr], null, 4),
             // );
 
             roonState = handleZonesSeekChanged(roonState, data[attr]);
+
+            // console.log(
+            //   'roon_state.js: emitting zonesSeekChanged message: buildZonesSeekChangedMessage(data[attr]):',
+            //   JSON.stringify(buildZonesSeekChangedMessage(data[attr]), null, 4),
+            // );
+
             io.emit(
               'zonesSeekChanged',
               buildZonesSeekChangedMessage(data[attr]),
             );
             break;
-          default:
-            // console.log('roon_state.js: unknown message: attr:', attr);
+
+          case 'zonesChanged':
             // console.log(
-            //   'roon_state.js: unknown message: data[attr]',
-            //   data[attr],
+            //   'roon_state.js: processing zonesChanged message: data[attr]:',
+            //   JSON.stringify(data[attr], null, 4),
             // );
+
+            console.log(
+              'roon_state.js: emitting zonesChanged message: buildZonesChangedMessage(data[attr]):',
+              JSON.stringify(buildZonesChangedMessage(data[attr], null, 4)),
+            );
+
+            io.emit('zonesChanged', buildZonesChangedMessage(data[attr]));
+            break;
+
+          default:
+            console.log(
+              'roon_state.js: unknown message: attr:',
+              JSON.stringify(attr, null, 4),
+            );
+            console.log(
+              'roon_state.js: unknown message: data[attr]',
+              JSON.stringify(data[attr], null, 4),
+            );
 
             break;
         }
@@ -136,6 +160,23 @@ const buildZonesSeekChangedMessage = (coreMsg) => {
         {
           seekPosition: zoneData.seekPosition,
           queueTimeRemaining: zoneData.queueTimeRemaining,
+          zoneId: zoneData.zoneId,
+        },
+      ];
+    }),
+  );
+};
+
+const buildZonesChangedMessage = (coreMsg) => {
+  return Object.fromEntries(
+    coreMsg.map((zoneData) => {
+      return [
+        zoneData.zoneId,
+        {
+          displayName: zoneData.displayName,
+          nowPlaying: zoneData.nowPlaying ? zoneData.nowPlaying : null,
+          queueTimeRemaining: zoneData.queueTimeRemaining,
+          state: zoneData.state,
           zoneId: zoneData.zoneId,
         },
       ];
