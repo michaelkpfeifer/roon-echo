8edf7f629a787f5cfbb74e9381c114a5f8a38a08
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-11-12 20:30:28 +0100

    Get ESLint working even with a deprecated configuration

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    module: true,
+    process: true,
+  },
+  plugins: ['import'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'always',
+      },
+    ],
+
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
