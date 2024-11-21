a984fd5bb4a5e33c7ffacbef9e252941e1670fda
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-11-21 13:31:30 +0100

    Move zone selection to its own component

--- frontend/src/NowPlaying/Zones.jsx
+++ frontend/src/NowPlaying/Zones.jsx
@@ -1,37 +1,16 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 
 import AppContext from '../AppContext';
-import Modal from '../Modal';
 import SelectedZone from './SelectedZone';
+import ZoneSelection from './ZoneSelection';
 
 function Zones() {
-  const { appState, roonState, setAppState, setConfig } =
-    useContext(AppContext);
+  const { setAppState } = useContext(AppContext);
 
   const openModal = () =>
     setAppState((currentAppState) => ({
       ...currentAppState,
       isZonesModalOpen: true,
-    }));
-
-  const closeModal = () =>
-    setAppState((currentAppState) => ({
-      ...currentAppState,
-      isZonesModalOpen: false,
-    }));
-
-  const isZoneSelected = (zoneId) => appState.tmpSelectedZoneId === zoneId;
-
-  const handleZoneSelection = (zoneId) =>
-    setAppState((currentAppState) => ({
-      ...currentAppState,
-      tmpSelectedZoneId: zoneId,
-    }));
-
-  const handleConfirm = () =>
-    setConfig((currentConfig) => ({
-      ...currentConfig,
-      selectedZoneId: appState.tmpSelectedZoneId,
     }));
 
   return (
@@ -40,33 +19,7 @@ function Zones() {
       <button type="button" onClick={openModal}>
         Zones
       </button>
-      <Modal
-        isOpen={appState.isZonesModalOpen}
-        onClose={closeModal}
-        onConfirm={handleConfirm}
-      >
-        {Object.values(roonState.zones).map((zone) => (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-            key={zone.zoneId}
-          >
-            <span style={{ flex: 5 }}>{zone.displayName}</span>
-            <span style={{ flex: 2 }}>{zone.state}</span>
-            <span style={{ flex: 2 }}>{zone.queueTimeRemaining}</span>
-            <span style={{ flex: 1 }}>
-              <input
-                type="checkbox"
-                checked={isZoneSelected(zone.zoneId)}
-                onChange={() => handleZoneSelection(zone.zoneId)}
-              />
-            </span>
-          </div>
-        ))}
-      </Modal>
+      <ZoneSelection />
     </>
   );
 }
