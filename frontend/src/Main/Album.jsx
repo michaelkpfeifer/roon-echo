f61dd7e6849d53c83d8a38caf967ae6f3d80df1d
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-01-28 19:57:01 +0100

    Show tracks of an album

--- frontend/src/Main/Album.jsx
+++ frontend/src/Main/Album.jsx
@@ -15,6 +15,16 @@ function Album() {
     <>
       <h1>Album</h1>
       <h2>{album.mbAlbum.albumName}</h2>
+      {album.mbAlbumTracks.map((track) => (
+        <div key={track.id} className="album-track-row">
+          <div className="track-row__name">
+            <b>{track.name}</b>
+          </div>
+          <div className="album-track-row__track-add-next">
+            <button type="button">Play Next</button>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
