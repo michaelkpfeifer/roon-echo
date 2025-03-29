fbed6f17970910139353fb3734c5ced0f4bc293a
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-03-29 16:27:55 +0100

    Ad the toIso8601 function

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -30,4 +30,16 @@ const snakeCaseKeys = (obj) => {
   return obj;
 };
 
-export { camelCaseKeys, snakeCaseKeys };
+const toIso8601 = (ts) => {
+  const year = ts.getFullYear();
+  const month = String(ts.getMonth() + 1).padStart(2, '0');
+  const day = String(ts.getDate()).padStart(2, '0');
+  const hours = String(ts.getHours()).padStart(2, '0');
+  const minutes = String(ts.getMinutes()).padStart(2, '0');
+  const seconds = String(ts.getSeconds()).padStart(2, '0');
+  const milliseconds = String(ts.getMilliseconds()).padStart(3, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
+
+export { camelCaseKeys, snakeCaseKeys, toIso8601 };
