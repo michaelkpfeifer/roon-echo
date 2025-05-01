f7e48e7acaeccca045ad69afe12253bb36fc6063
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-05-01 08:49:11 +0200

    Add type and score columns, refer to roon_album_id of roon_albums table

--- backend/migrations/20250112223902_create_albums_table.js
+++ backend/migrations/20250112223902_create_albums_table.js
@@ -5,14 +5,17 @@
 export function up(knex) {
   return knex.schema.createTable('albums', (table) => {
     table.text('mb_album_id').notNullable().primary();
-    table.text('roon_artist_name').notNullable();
-    table.text('roon_album_name').notNullable();
+    table
+      .integer('roon_album_id')
+      .notNullable()
+      .references('id')
+      .inTable('roon_albums')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table.text('type').notNullable();
+    table.integer('score');
+    table.integer('track_count');
     table.text('mb_release_date');
-
-    table.unique(
-      ['roon_artist_name', 'roon_album_name'],
-      'albums_unique_on_artist_name_album_name',
-    );
 
     table.timestamps(true, true);
   });
