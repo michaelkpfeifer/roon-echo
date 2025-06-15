72ed9e4adcbbfdcd66a8ce66b519b8fe5446051b
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-06-15 17:40:43 +0200

    Add tests for camelCaseKeys

--- backend/__tests__/utils.test.js
+++ backend/__tests__/utils.test.js
@@ -1,9 +1,57 @@
-import { toIso8601 } from '../src/utils.js';
+import { camelCaseKeys, toIso8601 } from '../src/utils.js';
 
 describe('toIso8601', () => {
   test('converts to ISO 8601 format', () => {
     const date = new Date(2025, 4, 13, 17, 19, 23, 29);
 
     expect(toIso8601(date)).toEqual('2025-05-13 17:19:23.029');
+  });
+});
+
+describe('camelCaseKeys', () => {
+  test('returns an empty object when given an empty object', () => {
+    const obj = {};
+
+    const newObj = camelCaseKeys(obj);
+
+    expect(newObj).toEqual(obj);
+  });
+
+  test('converts the key to camelCase in a simple case', () => {
+    const obj = { key_one: 'value_one' };
+
+    const newObj = camelCaseKeys(obj);
+
+    expect(newObj).toEqual({ keyOne: 'value_one' });
+  });
+
+  test('leves camelCase keys alone', () => {
+    const obj = { keyOne: 'value_one' };
+
+    const newObj = camelCaseKeys(obj);
+
+    expect(newObj).toEqual(obj);
+  });
+
+  test('works correctly for objects stored in arrays', () => {
+    const ary = ['val', { key_one: 'value_one' }];
+
+    const newAry = camelCaseKeys(ary);
+
+    expect(newAry).toEqual(['val', { keyOne: 'value_one' }]);
+  });
+
+  test('works correctly for nested objects', () => {
+    const obj = {
+      key_one: 'value_one',
+      key_two: { key_three: 'value_three' },
+    };
+
+    const newObj = camelCaseKeys(obj);
+
+    expect(newObj).toEqual({
+      keyOne: 'value_one',
+      keyTwo: { keyThree: 'value_three' },
+    });
   });
 });
