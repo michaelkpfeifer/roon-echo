5106ad7fdb4e81971426560dae9dcc691db3c986
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-06-15 17:47:47 +0200

    Add tests for snakeCaseKeys

--- backend/__tests__/utils.test.js
+++ backend/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { camelCaseKeys, toIso8601 } from '../src/utils.js';
+import { camelCaseKeys, snakeCaseKeys, toIso8601 } from '../src/utils.js';
 
 describe('toIso8601', () => {
   test('converts to ISO 8601 format', () => {
@@ -52,6 +52,54 @@ describe('camelCaseKeys', () => {
     expect(newObj).toEqual({
       keyOne: 'value_one',
       keyTwo: { keyThree: 'value_three' },
+    });
+  });
+});
+
+describe('snakeCaseKeys', () => {
+  test('returns an empty object when given an empty object', () => {
+    const obj = {};
+
+    const newObj = snakeCaseKeys(obj);
+
+    expect(newObj).toEqual(obj);
+  });
+
+  test('converts the key to camelCase in a simple case', () => {
+    const obj = { keyOne: 'value_one' };
+
+    const newObj = snakeCaseKeys(obj);
+
+    expect(newObj).toEqual({ key_one: 'value_one' });
+  });
+
+  test('leves snakeCase keys alone', () => {
+    const obj = { key_one: 'value_one' };
+
+    const newObj = snakeCaseKeys(obj);
+
+    expect(newObj).toEqual(obj);
+  });
+
+  test('works correctly for objects stored in arrays', () => {
+    const ary = ['val', { keyOne: 'value_one' }];
+
+    const newAry = snakeCaseKeys(ary);
+
+    expect(newAry).toEqual(['val', { key_one: 'value_one' }]);
+  });
+
+  test('works correctly for nested objects', () => {
+    const obj = {
+      keyOne: 'value_one',
+      keyTwo: { keyThree: 'value_three' },
+    };
+
+    const newObj = snakeCaseKeys(obj);
+
+    expect(newObj).toEqual({
+      key_one: 'value_one',
+      key_two: { key_three: 'value_three' },
     });
   });
 });
