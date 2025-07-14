71183c6a24fea17027e09d0eaa5e464119d12348
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-07-14 22:10:08 +0200

    Make test for extractQueueItems pass after TypeScript introduction

--- backend/__tests__/queues.test.ts
+++ backend/__tests__/queues.test.ts
@@ -7,96 +7,96 @@ describe('extractQueueItems', () => {
     expect(one).toEqual(one)
   });
 
-  // const items = [
-  //   {
-  //     queueItemId: 846769,
-  //     length: 479,
-  //     imageKey: '5ddef6d2e2d390c25db25aad80ae9c87',
-  //     oneLine: {
-  //       line1: 'Hospital Chapel - Burial',
-  //     },
-  //     twoLine: {
-  //       line1: 'Hospital Chapel',
-  //       line2: 'Burial',
-  //     },
-  //     threeLine: {
-  //       line1: 'Hospital Chapel',
-  //       line2: 'Burial',
-  //       line3: 'Streetlands EP',
-  //     },
-  //   },
-  // ];
+  const items = [
+    {
+      queueItemId: 846769,
+      length: 479,
+      imageKey: '5ddef6d2e2d390c25db25aad80ae9c87',
+      oneLine: {
+        line1: 'Hospital Chapel - Burial',
+      },
+      twoLine: {
+        line1: 'Hospital Chapel',
+        line2: 'Burial',
+      },
+      threeLine: {
+        line1: 'Hospital Chapel',
+        line2: 'Burial',
+        line3: 'Streetlands EP',
+      },
+    },
+  ];
 
-  // test('returns items if items key is present', () => {
-  //   const queue = {
-  //     items,
-  //   };
+  test('returns items if items key is present', () => {
+    const queue = {
+      items,
+    };
 
-  //   const queueItems = extractQueueItems(queue);
+    const queueItems = extractQueueItems(queue);
 
-  //   expect(queueItems).toEqual(items);
-  // });
+    expect(queueItems).toEqual(items);
+  });
 
-  // test('returns items if no remove operation in queue', () => {
-  //   const queue = {
-  //     changes: [
-  //       {
-  //         operation: 'insert',
-  //         index: 0,
-  //         items,
-  //       },
-  //     ],
-  //   };
+  test('returns items if no remove operation in queue', () => {
+    const queue = {
+      changes: [
+        {
+          operation: 'insert',
+          index: 0,
+          items,
+        },
+      ],
+    };
 
-  //   const queueItems = extractQueueItems(queue);
+    const queueItems = extractQueueItems(queue);
 
-  //   expect(queueItems).toEqual(items);
-  // });
+    expect(queueItems).toEqual(items);
+  });
 
-  // test('returns items if remove and insert operations in queue', () => {
-  //   const queue = {
-  //     changes: [
-  //       {
-  //         operation: 'remove',
-  //         index: 0,
-  //         count: 2,
-  //       },
-  //       {
-  //         operation: 'insert',
-  //         index: 0,
-  //         items,
-  //       },
-  //     ],
-  //   };
+  test('returns items if remove and insert operations in queue', () => {
+    const queue = {
+      changes: [
+        {
+          operation: 'remove',
+          index: 0,
+          count: 2,
+        },
+        {
+          operation: 'insert',
+          index: 0,
+          items,
+        },
+      ],
+    };
 
-  //   const queueItems = extractQueueItems(queue);
+    const queueItems = extractQueueItems(queue);
 
-  //   expect(queueItems).toEqual(items);
-  // });
+    expect(queueItems).toEqual(items);
+  });
 
-  // test('returns an empty list if all items are removed', () => {
-  //   const queue = {
-  //     changes: [
-  //       {
-  //         operation: 'remove',
-  //         index: 0,
-  //         count: 5,
-  //       },
-  //     ],
-  //   };
+  test('returns an empty list if all items are removed', () => {
+    const queue = {
+      changes: [
+        {
+          operation: 'remove',
+          index: 0,
+          count: 5,
+        },
+      ],
+    };
 
-  //   const queueItems = extractQueueItems(queue);
+    const queueItems = extractQueueItems(queue);
 
-  //   expect(queueItems).toEqual([]);
-  // });
+    expect(queueItems).toEqual([]);
+  });
 
-  // test('throws error if queue has unexpected shape', () => {
-  //   const queue = {
-  //     somethingElse: items,
-  //   };
+  test('throws error if queue has unexpected shape', () => {
+    const queue = {
+      somethingElse: items,
+    };
 
-  //   expect(() => extractQueueItems(queue)).toThrow(
-  //     'Error: Cannot extract items from queue',
-  //   );
-  // });
+    expect(() => extractQueueItems(queue)).toThrow(
+      'Error: Cannot extract items from queue',
+    );
+  });
 });
