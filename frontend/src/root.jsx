35a4ba130cd708a60e294d6ed621e88b11d5a661
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-10-11 08:59:55 +0200

    Extract CSS for Albums component to its own file

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './styles/global.css';
+import './styles/global/albums.css';
 import './styles/global/album.css';
 import './styles/global/sidebar.css';
 
