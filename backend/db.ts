7292ee98a860ee3ba43336b56cfb101785b78550
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-12-10 23:07:34 +0100

    Fix Knex configuration

--- backend/db.ts
+++ backend/db.ts
@@ -3,7 +3,8 @@ import knexConfig from './knexfile.js';
 import type { DatabaseSchema } from './databaseSchema';
 import type { Knex } from 'knex';
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = (process.env.NODE_ENV ||
+  'development') as keyof typeof knexConfig;
 const untypedDb = knexInit(knexConfig[environment]);
 const db: Knex<DatabaseSchema> = untypedDb;
 
