290a0bff94afcc3531dfa10ec8c44e323b7e2ead
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-01-05 21:19:01 +0100

    Show queue for the currently selected zone only

--- frontend/src/Main/Queues.jsx
+++ frontend/src/Main/Queues.jsx
@@ -4,35 +4,33 @@ import AppContext from '../AppContext';
 import { lookupZoneName } from '../utils';
 
 function Queues() {
-  const { appState, roonState } = useContext(AppContext);
-
-  /* eslint-disable no-console */
-  console.log('Queues.jsx: Queues(): appstate.queues', appState.queues);
-  /* eslint-enable no-console */
+  const { appState, config, roonState } = useContext(AppContext);
 
   return (
     <>
       <h1 className="heading-display">Queues</h1>
       <div className="queues-container">
-        {Object.entries(appState.queues).map(([zoneId, queue]) => (
-          <div key={zoneId}>
-            <h2>{lookupZoneName(roonState.zones, zoneId)}</h2>
+        {config.selectedZoneId && appState.queues[config.selectedZoneId] && (
+          <div>
+            <h2>{lookupZoneName(roonState.zones, config.selectedZoneId)}</h2>
             <table>
               <tbody>
-                {queue.map(({ queueItemId, length, imageKey, threeLine }) => (
-                  <tr key={queueItemId}>
-                    <td>{queueItemId}</td>
-                    <td>{length}</td>
-                    <td>{imageKey}</td>
-                    <td>{threeLine.line1}</td>
-                    <td>{threeLine.line2}</td>
-                    <td>{threeLine.line3}</td>
-                  </tr>
-                ))}
+                {appState.queues[config.selectedZoneId].map(
+                  ({ queueItemId, length, imageKey, threeLine }) => (
+                    <tr key={queueItemId}>
+                      <td>{queueItemId}</td>
+                      <td>{length}</td>
+                      <td>{imageKey}</td>
+                      <td>{threeLine.line1}</td>
+                      <td>{threeLine.line2}</td>
+                      <td>{threeLine.line3}</td>
+                    </tr>
+                  ),
+                )}
               </tbody>
             </table>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
