3525c83329033b5ecbe2f4e520cf93b45f5ea5ff
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-01-05 22:39:25 +0100

    Replace table layout for queues by a simple grid layout

--- frontend/src/Main/Queues.jsx
+++ frontend/src/Main/Queues.jsx
@@ -4,31 +4,40 @@ import AppContext from '../AppContext';
 import { lookupZoneName } from '../utils';
 
 function Queues() {
-  const { appState, config, roonState } = useContext(AppContext);
+  const { appState, config, coreUrlRef, roonState } = useContext(AppContext);
+  const coreUrl = coreUrlRef.current;
 
   return (
     <>
       <h1 className="heading-display">Queues</h1>
-      <div className="queues-container">
+      <div className="queue-container">
         {config.selectedZoneId && appState.queues[config.selectedZoneId] && (
-          <div>
-            <h2>{lookupZoneName(roonState.zones, config.selectedZoneId)}</h2>
-            <table>
-              <tbody>
-                {appState.queues[config.selectedZoneId].map(
-                  ({ queueItemId, length, imageKey, threeLine }) => (
-                    <tr key={queueItemId}>
-                      <td>{queueItemId}</td>
-                      <td>{length}</td>
-                      <td>{imageKey}</td>
-                      <td>{threeLine.line1}</td>
-                      <td>{threeLine.line2}</td>
-                      <td>{threeLine.line3}</td>
-                    </tr>
-                  ),
-                )}
-              </tbody>
-            </table>
+          <div className="queue-heading">
+            <div className="queue-heading__name">
+              {lookupZoneName(roonState.zones, config.selectedZoneId)}
+            </div>
+            <div>
+              {appState.queues[config.selectedZoneId].map(
+                ({ queueItemId, length, imageKey, threeLine }) => (
+                  <div className="queue-item" key={queueItemId}>
+                    <div className="queue-item__image">
+                      <img
+                        className="queue-item__image-display"
+                        src={`${coreUrl}/api/image/${imageKey}?scale=fit&width=150&height=150`}
+                        alt={threeLine.line1}
+                      />
+                    </div>
+                    <div className="queue-item__queue-item-id">
+                      {queueItemId}
+                    </div>
+                    <div className="queue-item__line-1">{threeLine.line1}</div>
+                    <div className="queue-item__line-2">{threeLine.line2}</div>
+                    <div className="queue-item__line-3">{threeLine.line3}</div>
+                    <div className="queue-item__length">{length}</div>
+                  </div>
+                ),
+              )}
+            </div>
           </div>
         )}
       </div>
