37ea2a9bf6b10277045ab0865813110e2c711e4b
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-01-16 18:29:08 +0100

    Add PersistedRoonAlbum type

---
+++ shared/internal/persistedRoonAlbum.ts
@@ -0,0 +1,11 @@
+type PersistedRoonAlbum = {
+  roonAlbumId: string;
+  albumName: string;
+  artistName: string;
+  candidatesFetchedAt?: string;
+  candidatesMatchedAt?: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export { PersistedRoonAlbum };
