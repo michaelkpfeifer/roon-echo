0ee544b4c8baca475d76f9fc2b6532abb9f64175
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-01-30 17:29:00 +0100

    Add internal Play type

---
+++ shared/internal/play.ts
@@ -0,0 +1,16 @@
+type Play = {
+  id: string;
+  roon_track_id: string;
+  roon_album_id: string;
+  roon_queue_item_id: number;
+  roon_album_name: string;
+  roon_artist_name: string;
+  roon_track_name: string;
+  number: string;
+  position: number;
+  played_at: string;
+  fraction_played: number;
+  is_played: boolean;
+};
+
+export { Play };
