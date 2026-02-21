fe1c373f805130d7c266eb3e381e3b3ac0e8e210
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-02-21 15:10:01 +0100

    Build join table between albums and MuiscBrainz artists

--- backend/migrations/20250202100134_create_albums_mb_artists_table.js
+++ backend/migrations/20250202100134_create_albums_mb_artists_table.js
@@ -4,31 +4,29 @@
  */
 export async function up(knex) {
   await knex.raw(`
-    CREATE TABLE mb_albums_mb_artists (
-      mb_album_id TEXT NOT NULL
-        CHECK (length(mb_album_id) = 36),
-      mb_artist_id TEXT NOT NULL
-        CHECK (length(mb_artist_id) = 36),
+    CREATE TABLE albums_mb_artists (
       album_id TEXT NOT NULL
         CHECK (length(album_id) = 36),
+      mb_artist_id TEXT NOT NULL
+        CHECK (length(mb_artist_id) = 36),
       position INTEGER NOT NULL,
       joinphrase TEXT NOT NULL,
       created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
       updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY(mb_album_id, album_id) REFERENCES mb_albums(mb_album_id, album_id)
+      FOREIGN KEY(album_id) REFERENCES albums(album_id)
         ON DELETE CASCADE,
       FOREIGN KEY(mb_artist_id) REFERENCES mb_artists(mb_artist_id)
         ON DELETE CASCADE,
-      CONSTRAINT mb_albums_mb_artists_primary_key PRIMARY KEY (mb_album_id, album_id, mb_artist_id)
+      CONSTRAINT albums_mb_artists_primary_key PRIMARY KEY (album_id, mb_artist_id)
     );
   `);
 
   await knex.raw(`
-    CREATE INDEX mb_albums_mb_artists_on_mb_artist_id ON mb_albums_mb_artists (mb_artist_id);
+    CREATE INDEX albums_mb_artists_on_mb_artist_id ON albums_mb_artists (mb_artist_id);
   `);
 
   await knex.raw(`
-    CREATE INDEX mb_albums_mb_artists_on_mb_album_id ON mb_albums_mb_artists (mb_album_id);
+    CREATE INDEX albums_mb_artists_on_album_id ON albums_mb_artists (album_id);
   `);
 }
 
@@ -37,5 +35,5 @@ export async function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  return knex.schema.dropTable('mb_albums_mb_artists');
+  return knex.schema.dropTable('albums_mb_artists');
 }
