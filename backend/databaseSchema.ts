c75afb6f61c86cccce3e179641f2f6f8b39adb76
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-02-21 17:06:42 +0100

    Remove roon_albums and mb_albums from database schema

--- backend/databaseSchema.ts
+++ backend/databaseSchema.ts
@@ -23,16 +23,6 @@ type DatabaseSchema = {
     mb_release_date: string | null;
   };
 
-  roon_albums: {
-    roon_album_id: string;
-    album_name: string;
-    artist_name: string;
-    mb_candidates_fetched_at: string | null;
-    mb_candidates_matched_at: string | null;
-    created_at: string;
-    updated_at: string;
-  };
-
   roon_tracks: {
     roon_track_id: string;
     album_id: string;
@@ -48,17 +38,6 @@ type DatabaseSchema = {
     name: string;
     sort_name: string;
     disambiguation: string | null;
-    created_at: string;
-    updated_at: string;
-  };
-
-  mb_albums: {
-    mb_album_id: string;
-    album_id: string;
-    album_name: string;
-    score: number | null;
-    track_count: number | null;
-    release_date: string | null;
     created_at: string;
     updated_at: string;
   };
