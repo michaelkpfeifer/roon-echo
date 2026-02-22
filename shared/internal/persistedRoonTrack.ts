6ffd2123b6a3032dfd0ac5bd7568d6e2cad1df62
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-02-22 11:13:47 +0100

    Add PersistedRoonTrack type

---
+++ shared/internal/persistedRoonTrack.ts
@@ -0,0 +1,11 @@
+type PersistedRoonTrack = {
+  trackId: string;
+  albumId: string;
+  roonTrackName: string;
+  roonNumber: string;
+  roonPosition: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type { PersistedRoonTrack };
