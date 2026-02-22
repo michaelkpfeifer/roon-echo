9eedeff33cadc08d08a53bbb961a9f56fda8a1c1
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-02-22 11:17:36 +0100

    Add createRoonAlbum function to test factories

--- backend/__factories__/roonAlbumFactory.ts
+++ backend/__factories__/roonAlbumFactory.ts
@@ -1,4 +1,9 @@
+import type { Knex } from 'knex';
+import fp from 'lodash/fp.js';
+
 import type { RoonAlbum } from '../../shared/internal/roonAlbum.js';
+import type { DatabaseSchema } from '../databaseSchema.js';
+import { snakeCaseKeys } from '../src/utils.js';
 
 const buildRoonAlbum = (overrides?: Partial<RoonAlbum>): RoonAlbum => ({
   albumId: '019b8914-76dc-7470-a65e-0b2b45d74489',
@@ -11,4 +16,25 @@ const buildRoonAlbum = (overrides?: Partial<RoonAlbum>): RoonAlbum => ({
   ...overrides,
 });
 
-export { buildRoonAlbum };
+const createRoonAlbum = async (
+  db: Knex<DatabaseSchema>,
+  overrides?: Partial<RoonAlbum>,
+): Promise<RoonAlbum> => {
+  const roonAlbum: RoonAlbum = {
+    albumId: '019b8914-76dc-7470-a65e-0b2b45d74489',
+    roonAlbumName: 'Default Album',
+    roonAlbumArtistName: 'Default Artist',
+    itemKey: '1357:4',
+    imageKey: 'dc7f2533fb475c3d3e80fce4ef7b2294',
+    mbCandidatesMatchedAt: null,
+    mbCandidatesFetchedAt: null,
+    ...overrides,
+  };
+
+  await db('albums').insert(
+    snakeCaseKeys(fp.omit(['itemKey', 'imageKey'], roonAlbum)),
+  );
+  return roonAlbum;
+};
+
+export { buildRoonAlbum, createRoonAlbum };
