fd47a0b34374eed42097de18cb1b2b46061e3453
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-03-01 17:40:43 +0100

    Add createMbArtist function to mbArtistFactory

--- backend/__factories__/mbArtistFactory.ts
+++ backend/__factories__/mbArtistFactory.ts
@@ -1,4 +1,8 @@
+import type { Knex } from 'knex';
+
+import type { DatabaseSchema } from '../databaseSchema.js';
 import type { MbArtist } from '../../shared/internal/mbArtist.js';
+import { snakeCaseKeys } from '../src/utils.js';
 
 const buildMbArtist = (overrides?: Partial<MbArtist>): MbArtist => ({
   mbArtistId: '019b8afe-c1b4-74f5-80ba-f025cecacfdd',
@@ -18,4 +22,23 @@ const buildMbArtists = (count: number): MbArtist[] => {
   return numbers.map((n) => buildNthMbArtist(n));
 };
 
-export { buildMbArtist, buildMbArtists };
+const createMbArtist = async (
+  db: Knex<DatabaseSchema>,
+  overrides?: Partial<MbArtist>,
+): Promise<MbArtist> => {
+  const mbArtist = {
+    mbArtistId: '019ca520-200b-75b8-b83c-bc28d1ce3816',
+    name: 'Test Artist',
+    sortName: 'Artist, Test',
+    disambiguation: 'Test Disambiguation',
+    ...overrides,
+  };
+
+  await db<DatabaseSchema['mb_artists']>('mb_artists').insert(
+    snakeCaseKeys(mbArtist),
+  );
+
+  return mbArtist;
+};
+
+export { buildMbArtist, buildMbArtists, createMbArtist };
