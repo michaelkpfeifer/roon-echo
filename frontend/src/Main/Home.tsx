d5514672a9f00e79cadd7bedce3d34e0047dccb3
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-04-22 22:25:36 +0200

    Show number of artists, albums, and tracks on home page

--- frontend/src/Main/Home.tsx
+++ frontend/src/Main/Home.tsx
@@ -1,5 +1,67 @@
+import { Disc, Music4, User } from 'lucide-react';
+import { useContext } from 'react';
+
+import AppContext from '../AppContext';
+
 function Home() {
-  return <h1 className="heading-display">Home</h1>;
+  const { albumAggregates } = useContext(AppContext);
+
+  const albumsCount = albumAggregates.length;
+
+  const artistsCount = [
+    ...new Set(
+      albumAggregates
+        .filter(
+          (albumAggregate) =>
+            albumAggregate.stage != 'empty' &&
+            albumAggregate.stage != 'withRoonAlbum',
+        )
+        .map((albumAggregate) => albumAggregate.roonAlbum.roonAlbumArtistName),
+    ),
+  ].length;
+
+  const tracksCount = albumAggregates
+    .filter(
+      (albumAggregate) =>
+        albumAggregate.stage != 'empty' &&
+        albumAggregate.stage != 'withRoonAlbum',
+    )
+    .map((albumAggregate) => albumAggregate.roonTracks.length)
+    .reduce((acc, n) => acc + n, 0);
+
+  return (
+    <div className="main-container">
+      <div className="count-cards-container">
+        <div className="count-card">
+          <div>
+            <User className="count-card__icon" />
+          </div>
+          <div className="count-card__data">
+            <div className="count-card__count">{artistsCount}</div>
+            <div className="count-card__item-name">Artists</div>
+          </div>
+        </div>
+        <div className="count-card">
+          <div>
+            <Disc className="count-card__icon" />
+          </div>
+          <div className="count-card__data">
+            <div className="count-card__count">{albumsCount}</div>
+            <div className="count-card__item-name">Albums</div>
+          </div>
+        </div>
+        <div className="count-card">
+          <div>
+            <Music4 className="count-card__icon" />
+          </div>
+          <div className="count-card__data">
+            <div className="count-card__count">{tracksCount}</div>
+            <div className="count-card__item-name">Tracks</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './styles/global.css';
-import './styles/global/sidebar.css';
-import './styles/global/albums.css';
 import './styles/global/album-card.css';
 import './styles/global/album.css';
-import './styles/global/track-row.css';
+import './styles/global/albums.css';
+import './styles/global/artists.css';
+import './styles/global/main.css';
 import './styles/global/queue.css';
+import './styles/global/sidebar.css';
+import './styles/global/track-row.css';
+import './styles/global/tracks.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Cannot find root element.');
