d9d8c19826913fcead77050070a1dc6b31f36be1
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-05-04 20:58:47 +0200

    Get rid of SelectedZone component that has become obsolete

--- frontend/src/NowPlaying.tsx
+++ frontend/src/NowPlaying.tsx
@@ -1,7 +1,6 @@
 import Controls from './NowPlaying/Controls';
 import Cover from './NowPlaying/Cover';
 import Volume from './NowPlaying/Volume';
-import SelectedZone from './NowPlaying/SelectedZone';
 
 function NowPlaying() {
   return (
@@ -12,10 +11,7 @@ function NowPlaying() {
       <div className="controls">
         <Controls />
       </div>
-      <div className="zones-and-volume">
-        <span>
-          <SelectedZone />
-        </span>
+      <div className="zones">
         <span>
           <Volume />
         </span>
