cf7067663f42be3ba6706a083ce188446871a7dc
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-05-04 20:36:53 +0200

    Get rid of zone selection in modal

--- frontend/src/NowPlaying/Zones.tsx
+++ frontend/src/NowPlaying/Zones.tsx
@@ -1,27 +1,7 @@
-import { useContext } from 'react';
-
-import AppContext from '../AppContext';
 import SelectedZone from './SelectedZone';
-import ZoneSelection from './ZoneSelection';
 
 function Zones() {
-  const { setAppState } = useContext(AppContext);
-
-  const openModal = () =>
-    setAppState((currentAppState) => ({
-      ...currentAppState,
-      isZonesModalOpen: true,
-    }));
-
-  return (
-    <>
-      <SelectedZone />
-      <button type="button" onClick={openModal}>
-        Zones
-      </button>
-      <ZoneSelection />
-    </>
-  );
+  return <SelectedZone />;
 }
 
 export default Zones;
