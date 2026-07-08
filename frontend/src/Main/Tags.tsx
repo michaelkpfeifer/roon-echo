eb496b37439caf19e0d96366bc2fa021a2865d36
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-07-08 22:56:21 +0200

    Add heading with button to create new tag and input field to filter

--- frontend/src/Main/Tags.tsx
+++ frontend/src/Main/Tags.tsx
@@ -1,9 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 function Tags() {
+  const [tagsPattern, setTagsPattern] = useState('');
+
   return (
     <>
       <h1 className="heading-display">Tags</h1>
+      <div className="tags-heading">
+        <div className="tags-heading__buttons">
+          <button type="button" className="tags-heading__button">
+            New Tag
+          </button>
+        </div>
+        <div className="tags-heading__filter">
+          <div className="filter">
+            <input
+              className="filter__input"
+              type="text"
+              value={tagsPattern}
+              onChange={(e) => setTagsPattern(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
