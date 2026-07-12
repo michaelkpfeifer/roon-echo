85a15321541394a34470db804ff25fd1722435e3
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-07-12 23:30:00 +0200

    Don't show volume string if nothing is queued

--- frontend/src/NowPlaying/Volume.tsx
+++ frontend/src/NowPlaying/Volume.tsx
@@ -1,4 +1,21 @@
+import { useContext } from 'react';
+
+import AppContext from '../AppContext';
+import { findSelectedZone } from '../utils';
+
 function Volume() {
+  const { config, zones } = useContext(AppContext);
+
+  const selectedZone = findSelectedZone(zones, config.selectedZoneId);
+
+  if (selectedZone === null) {
+    return null;
+  }
+
+  if (selectedZone.nowPlaying === null) {
+    return null;
+  }
+
   return <div>Volume</div>;
 }
 
