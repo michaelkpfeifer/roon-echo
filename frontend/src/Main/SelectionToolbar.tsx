3ffb0640d55bc7e2f6444437c1af5f2730387add
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2026-09-17 22:56:48 +0200

    Add Play button to selection toolbar

--- frontend/src/Main/SelectionToolbar.tsx
+++ frontend/src/Main/SelectionToolbar.tsx
@@ -7,12 +7,24 @@ function SelectionToolbar({
   selectedCount,
   onExitSelectionMode,
 }: SelectionToolbarProps) {
+  const hasSelection = selectedCount > 0;
+
   return (
     <>
       <div className="selection-toolbar">
         <span className="selection-toolbar__count">
           {selectedCount} album{selectedCount === 1 ? '' : 's'} selected
         </span>
+
+        <div className="selection-toolbar--split-button">
+          <button
+            className="selection-toolbar--play-now"
+            disabled={!hasSelection}
+          >
+            Play Now
+          </button>
+        </div>
+
         <button
           className="selection-toolbar--done"
           onClick={onExitSelectionMode}
